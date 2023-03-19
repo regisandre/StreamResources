185fcedd1479d3242aa0b7fbf28673fda2d1a28b
Sioxox <sioxox@pm.me>
2023-03-20 00:55:18 +0100

    Add README to explain how to add a gradient to a text using Photofiltre

---
+++ src/Tournoi League of Legends/Objet(s)/ressources/images/lol_panels/README.ml
@@ -0,0 +1,9 @@
+# How to gradient a text using Photofiltre?
+1. Add your text
+2. Create an empty layer
+3. Open the text options and push "Transform in selection"
+4. Select the empty layer
+5. Go to "Filters" > "Color" > "Gradient"
+6. Select the top direction in bichromatic mode
+7. Put the color 1 to opacity 0 and the color 2 to opacity 20 in black
+8. Click OK
